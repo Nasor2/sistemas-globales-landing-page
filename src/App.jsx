3ed0761546e700be9c9eebba4d52460e01cb1a82
Nasor2 <penaortegasamuel@gmail.com>
2025-02-22 15:11:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Hola munda</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
